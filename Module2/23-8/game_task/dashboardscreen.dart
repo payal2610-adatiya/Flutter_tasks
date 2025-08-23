789253057abede30ea02a811738bac67be0f25af
payal2610-adatiya <adatiapayal@gmail.com>
2025-08-23 14:56:58 +0530

    c

---
+++ Module2/23-8/game_task/dashboardscreen.dart
@@ -0,0 +1,209 @@
+import 'dart:async';
+import 'dart:core';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Dashboardscreen extends StatefulWidget {
+  const Dashboardscreen({super.key});
+
+  @override
+  State<Dashboardscreen> createState() => _DashboardscreenState();
+}
+
+class _DashboardscreenState extends State<Dashboardscreen> {
+  int current_index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: _logout, icon: Icon(Icons.logout))
+        ],
+      ),
+      body: current_index == 0 ? RandomNumberGame() : TapGame(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: current_index,
+        onTap: (index) => setState(() => current_index = index),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.casino), label: 'Random Number Game'),
+          BottomNavigationBarItem(icon: Icon(Icons.touch_app), label: 'Tap Challenge Game'),
+        ],
+      ),
+    );
+  }
+
+  _logout() {
+    Navigator.pop(context);
+  }
+}
+
+class RandomNumberGame extends StatefulWidget {
+  @override
+  _RandomNumberGameState createState() => _RandomNumberGameState();
+}
+
+class _RandomNumberGameState extends State<RandomNumberGame> {
+  final Random random = Random();
+  late int targetNumber;
+  int numberOfGuesses = 0;
+  final int maxGuesses = 5;
+  String hintMessage = '';
+  final TextEditingController guessController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _startNewGame();
+  }
+
+  void _startNewGame() {
+    setState(() {
+      targetNumber = random.nextInt(100) + 1;
+      numberOfGuesses = 0;
+      hintMessage = '';
+      guessController.clear();
+    });
+  }
+
+  void _checkGuess() {
+    final guess = int.tryParse(guessController.text);
+    if (guess == null) {
+      setState(() {
+        hintMessage = 'Please enter a valid number';
+      });
+      return;
+    }
+
+    numberOfGuesses++;
+
+    if (numberOfGuesses > maxGuesses) {
+      setState(() {
+        hintMessage = 'Game over! You have used all $maxGuesses guesses. The number was $targetNumber';
+      });
+      return;
+    }
+
+    if (guess > targetNumber) {
+      hintMessage = 'Too high!';
+    } else if (guess < targetNumber) {
+      hintMessage = 'Too low!';
+    } else {
+      hintMessage = 'Correct! You won in $numberOfGuesses guesses!';
+    }
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Random Number Game'),
+      ),
+      body: Column(
+        children: [
+          Text('Guess the number (1-100):'),
+          TextField(
+            controller: guessController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Enter your guess',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _checkGuess,
+            child: Text('Check Guess'),
+          ),
+          SizedBox(height: 20),
+          Text(hintMessage),
+        ],
+      ),
+    );
+  }
+}
+class TapGame extends StatefulWidget {
+  @override
+  _TapGameState createState() => _TapGameState();
+}
+
+class _TapGameState extends State<TapGame> {
+  int _tapCount = 0;
+  int _timeLeft = 10;
+  bool _gameStarted = false;
+
+  void _startGame() {
+    setState(() {
+      _tapCount = 0;
+      _timeLeft = 10;
+      _gameStarted = true;
+    });
+    Future.delayed(Duration(seconds: 10), _endGame);
+    _countDown();
+  }
+
+  void _countDown() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if (_timeLeft > 0) {
+        setState(() {
+          _timeLeft--;
+        });
+      } else {
+        timer.cancel();
+      }
+    });
+  }
+
+  void _incrementTap() {
+    if (_gameStarted) {
+      setState(() {
+        _tapCount++;
+      });
+    }
+  }
+
+  void _endGame() {
+    setState(() {
+      _gameStarted = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tap Challenge Game'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Tap as many times as possible in 10 seconds'),
+          GestureDetector(
+            onTap: () {
+              if (!_gameStarted) _startGame();
+              _incrementTap();
+            },
+            child: Container(
+              color: Colors.black12,
+              height: 100,
+              width: double.infinity,
+              child: Center(
+                child: Text(
+                  'TAP ME!',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text('Taps: $_tapCount', style: TextStyle(fontSize: 20)),
+          if (_gameStarted)
+            Text('Time Left: $_timeLeft seconds', style: TextStyle(fontSize: 20)),
+          if (!_gameStarted)
+            Text('Game Over!', style: TextStyle(fontSize: 20)),
+        ],
+      ),
+    );
+  }
+}
